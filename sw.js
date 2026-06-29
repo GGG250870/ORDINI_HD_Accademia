e92f4bd6116e53ba12eb88cf979368881ddffccc
GGG250870 <gianni.giro@gmail.com>
2026-06-29 10:11:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='hd-ordini-v3';
-const FILES=['./','./index.html','./ORDINIACCADEMIA_mobile.html','./style.css','./app.js','./app.webmanifest','./icon.svg'];
+const CACHE_NAME='hd-ordini-v5';
+const FILES=['./','./index.html','./style.css','./app.js','./prodotti.json','./app.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match('./index.html'))));});
